652b6d0dda53615e0a30e0a50ffe963d92f439ef
CesarMartinezUnab <mc1832012022@unab.edu.sv>
2025-04-14 18:08:30 -0500

    commit 21

--- backend/main.py
+++ backend/main.py
@@ -140,4 +140,4 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
